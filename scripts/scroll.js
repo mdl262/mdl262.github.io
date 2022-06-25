aca5cbf925462ad32583f2d011ed26909442dd9d
mdl262 <70067154+mdl262@users.noreply.github.com>
2022-06-25 14:54:57 -0400

    Add files via upload
    Mobile version 2.0

---
+++ scripts/scroll.js
@@ -0,0 +1,56 @@
+// by akinuri @ https://github.com/akinuri/scroll
+// some modifications by Max Li
+
+function Scroll(delay, query = null) {
+    // scroll event occurs if the page is refreshed when scrollY > 0
+    // this suggests that the browser scrolls to previous (before refresh) position
+    this.initialY = scrollY;
+    this.callbacks = {
+        start  : null,
+        scroll : null,
+        end    : null,
+    };
+    this.timeout = {
+        delay  : delay || 150,
+        handle : null,
+    };
+    this.handler = Scroll.handler.bind(this);
+    if (query) {
+        document.querySelectorAll(query)[0].addEventListener("scroll", this.handler);
+    } else {
+        window.addEventListener("scroll", this.handler);
+    }
+}
+
+Scroll.handler = function (e) {
+    if (this.initialY == 0) {
+        if (this.timeout.handle == null) {
+            if (this.callbacks.start) {
+                this.callbacks.start.call(this, e);
+            }
+        }
+        if (this.timeout.handle !== null) {
+            clearTimeout(this.timeout.handle);
+        }
+        if (this.callbacks.scroll) {
+            this.callbacks.scroll.call(this, e);
+        }
+        var self = this;
+        this.timeout.handle = setTimeout(function () {
+            self.timeout.handle = null;
+            if (self.callbacks.end) {
+                self.callbacks.end.call(self, e);
+            }
+        }, this.timeout.delay);
+    } else {
+        this.initialY = 0;
+    }
+};
+
+Scroll.prototype.addEventListener = function (event, callback) {
+    if (Object.keys(this.callbacks).includes(event)) {
+        this.callbacks[event] = callback;
+    } else {
+        console.warn("Invalid scroll event: " + event);
+    }
+};
